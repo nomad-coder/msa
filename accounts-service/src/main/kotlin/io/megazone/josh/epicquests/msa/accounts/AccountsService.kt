f09cd9e77f3d29aae62ad91c8f306f0dacdbf47f
Josh.hong <hong@mz.co.kr>
2020-01-07 18:17:42 +0900

    accounts test

--- accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
+++ accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
@@ -4,9 +4,19 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.context.config.annotation.RefreshScope
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters.fromValue
+import org.springframework.web.reactive.function.server.RequestPredicates.path
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.nest
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.notFound
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class AccountsService
@@ -15,22 +25,34 @@ fun main(args: Array<String>) {
 	runApplication<AccountsService>(*args)
 }
 
+@Configuration
+class RouteConfig(
+	private val handler: AccountsHandler
+) {
+
+	@Bean
+	fun routes(): RouterFunction<ServerResponse> {
+		return nest(path("/accounts"),
+			router {
+				GET("/", handler::list)
+				GET("/author", handler::author)
+			}
+		)
+	}
+
+}
+
 @RefreshScope
-@RestController
-@RequestMapping("/accounts")
-class AccountsController {
+@Component
+class AccountsHandler {
 
 	@Value("\${global.author}")
 	private val globalAuthor: String? = null
 
-	@Value("\${logging.level.org.springframework.web}")
-	private val springLoggingLevel: String? = null
+	fun author(request: ServerRequest): Mono<ServerResponse> = ok().body(fromValue(globalAuthor!!))
 
-	@GetMapping
-	fun list(): List<String> {
-		println(globalAuthor)
-		println(springLoggingLevel)
-		return listOf("josh", "yoon")
-	}
+	fun list(request: ServerRequest): Mono<ServerResponse> = ok()
+			.body(fromValue(listOf("Josh", "Yoon")))
+			.switchIfEmpty(notFound().build())
 
 }
