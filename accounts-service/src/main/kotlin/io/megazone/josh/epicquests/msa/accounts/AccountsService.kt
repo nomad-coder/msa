3e77647f2d3d1f0d8eafd4037937fc1f0e0a8dc4
Josh.hong <hong@mz.co.kr>
2020-01-07 17:55:52 +0900

    Actuator refresh 허용

--- accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
+++ accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
@@ -3,6 +3,7 @@ package io.megazone.josh.epicquests.msa.accounts
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +15,7 @@ fun main(args: Array<String>) {
 	runApplication<AccountsService>(*args)
 }
 
+@RefreshScope
 @RestController
 @RequestMapping("/accounts")
 class AccountsController {
