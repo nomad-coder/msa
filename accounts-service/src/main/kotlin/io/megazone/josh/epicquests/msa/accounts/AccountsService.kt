1b3015127e1dcfadec85a3a9952686de46943ceb
Josh.hong <hong@mz.co.kr>
2020-01-09 15:58:10 +0900

    불필요한 import 제거

--- accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
+++ accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
@@ -1,6 +1,5 @@
 package io.megazone.josh.epicquests.msa.accounts
 
-import org.h2.tools.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -8,7 +7,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters.fromValue
 import org.springframework.web.reactive.function.server.RequestPredicates.path
