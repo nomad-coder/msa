0ef0a2e97c645bbb99f5ef396a83f5f3b2004de2
Josh.hong <hong@mz.co.kr>
2020-01-09 14:24:06 +0900

    H2 실행방식 변경

--- accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
+++ accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/accounts/AccountsService.kt
@@ -1,5 +1,6 @@
 package io.megazone.josh.epicquests.msa.accounts
 
+import org.h2.tools.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,6 +8,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters.fromValue
 import org.springframework.web.reactive.function.server.RequestPredicates.path
@@ -25,6 +27,19 @@ class AccountsService
 
 fun main(args: Array<String>) {
 	runApplication<AccountsService>(*args)
+}
+
+@Configuration
+@Profile("local")
+class LocalConfig {
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	fun h2Server(): Server {
+		val server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092")
+
+		return server
+	}
+
 }
 
 @Configuration
