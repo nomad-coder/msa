83bef66b906ac88722713eac596506252f879e3b
Josh.hong <hong@mz.co.kr>
2020-01-10 12:06:23 +0900

    package name 변경 accounts -> account

---
+++ accounts-service/src/main/kotlin/io/megazone/josh/epicquests/msa/account/AccountsService.kt
@@ -0,0 +1,87 @@
+package io.megazone.josh.epicquests.msa.account
+
+import net.bytebuddy.utility.RandomString
+import org.hibernate.annotations.CreationTimestamp
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@EnableEurekaClient
+@SpringBootApplication
+class AccountsService
+
+fun main(args: Array<String>) {
+	runApplication<AccountsService>(*args)
+}
+
+@RestController
+@RequestMapping("/accounts")
+class AccountController(
+	private val repo: AccountRepository
+) {
+
+	@PostMapping
+	fun create(@RequestBody account: Account): Account {
+		return repo.save(account)
+	}
+
+	@GetMapping
+	fun list(): List<Account> {
+		return repo.findAll()
+	}
+
+	@GetMapping("/{id}")
+	fun details(@PathVariable id: String): Account {
+		return repo.findById(id).get()
+	}
+
+	@DeleteMapping("/{id}")
+	fun delete(@PathVariable id: String) {
+		return repo.deleteById(id)
+	}
+
+}
+
+@RefreshScope
+@RestController
+@RequestMapping("/test")
+class TestController(
+	@Value("\${global.author}") private val globalAuthor: String
+) {
+
+	@GetMapping("/author")
+	fun author(): String {
+		return globalAuthor
+	}
+
+}
+
+@Entity
+@Table(name = "ACCOUNTS")
+data class Account(
+
+	@Id
+	val id: String = RandomString.make(30),
+
+	val username: String? = null,
+
+	var password: String? = null,
+
+	var email: String? = null,
+
+	var name: String? = null,
+
+	@CreationTimestamp
+	val createdAt: Date? = Date()
+
+)
+
+interface AccountRepository : JpaRepository<Account, String>
